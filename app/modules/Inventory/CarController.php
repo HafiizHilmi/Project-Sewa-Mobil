5de0aa821c8ceda39c87d1fa5ed9dc4c41c9bc76
hafiizhilmi <hafiizhilmi@gmail.com>
2026-05-16 15:23:44 +0700

    Revisi Comment

---
+++ app/modules/Inventory/CarController.php
@@ -0,0 +1,9 @@
+<?php
+
+class CarController {
+    
+    public function index() {
+        
+        require_once __DIR__ . '/views/user_catalog.php';
+    }
+}
---
+++ public/index.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+
+$module = isset($_GET['module']) ? ucfirst($_GET['module']) : 'Inventory';
+$action = isset($_GET['action']) ? $_GET['action'] : 'index';
+
+$controllers = [
+    'Inventory' => 'CarController',
+    'Auth'      => 'AuthController',
+    'Booking'   => 'BookingController',
+    'Dashboard' => 'DashboardController',
+    'Verification'=> 'VerifyController'
+];
+
+$controllerName = isset($controllers[$module]) ? $controllers[$module] : $module . 'Controller';
+
+$controllerFile = __DIR__ . "/../app/modules/{$module}/{$controllerName}.php";
+
+if (file_exists($controllerFile)) {
+    require_once $controllerFile;
+    
+    if (class_exists($controllerName)) {
+        $controller = new $controllerName();
+        
+        if (method_exists($controller, $action)) {
+            $controller->$action();
+            
+        } else {
+            echo "<div style='text-align:center; margin-top:50px; font-family:sans-serif;'>
+                    <h2 style='color:#e11d48;'>Error 404: Halaman Tidak Ditemukan</h2>
+                    <p>Fungsi <b>{$action}()</b> tidak ditemukan di dalam <b>{$controllerName}</b>.</p>
+                  </div>";
+        }
+    } else {
+        echo "<h2 style='color:red; text-align:center;'>Error: Class {$controllerName} tidak ada di dalam filenya!</h2>";
+    }
+} else {
+    echo "<div style='text-align:center; margin-top:50px; font-family:sans-serif;'>
+            <h2 style='color:#e11d48;'>Error 404: Modul Tidak Ditemukan</h2>
+            <p>Sistem mencari file di path ini: <br><code style='background:#eee; padding:5px;'>{$controllerFile}</code></p>
+            <p>Pastikan nama folder dan file sudah benar.</p>
+          </div>";
+}
